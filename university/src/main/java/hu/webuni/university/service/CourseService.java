ac5e9a9755193bfcbc84b1fbc03a3f927fc4c543
imre-gabor <gabor@aut.bme.hu>
2022-02-03 20:36:37 +0100

    solution at live 1

--- university/src/main/java/hu/webuni/university/service/CourseService.java
+++ university/src/main/java/hu/webuni/university/service/CourseService.java
@@ -1,7 +1,18 @@
 package hu.webuni.university.service;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import hu.webuni.university.model.Course;
+import hu.webuni.university.model.QCourse;
 import hu.webuni.university.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +21,17 @@ import lombok.RequiredArgsConstructor;
 public class CourseService {
 	
 	private final CourseRepository courseRepository;
+	
+	@Transactional
+	public List<Course> searchWithRelationships(Predicate predicate, Pageable pageable){
+		
+//		List<Course> courses = courseRepository.findAll(predicate, "Course.students");
+//		courses = courseRepository.findAll(QCourse.course.in(courses) , "Course.teachers");
+		
+		List<Course> courses = courseRepository.findAll(predicate, pageable).getContent();
+		BooleanExpression inByCourseId = QCourse.course.in(courses);
+		courses = courseRepository.findAll(inByCourseId, "Course.teachers", Sort.unsorted());
+		courses = courseRepository.findAll(inByCourseId, "Course.students", pageable.getSort());
+		return courses;
+	}
 }
