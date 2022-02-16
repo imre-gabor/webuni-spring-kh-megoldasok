8f4afd5b309f1d863d20613484101f2cfee4720a
imre-gabor <gabor@aut.bme.hu>
2022-02-16 16:25:19 +0100

    solve week3/task1: generate controllers and dtos from openapi

--- university/src/main/java/hu/webuni/university/web/CourseController.java
+++ university/src/main/java/hu/webuni/university/web/CourseController.java
@@ -5,66 +5,83 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.data.web.SortDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.NativeWebRequest;
 
 import com.querydsl.core.types.Predicate;
 
-import hu.webuni.university.dto.CourseDto;
+import hu.webuni.airport.api.CourseControllerApi;
+import hu.webuni.airport.api.model.CourseDto;
+import hu.webuni.airport.api.model.HistoryDataCourseDto;
 import hu.webuni.university.mapper.CourseMapper;
 import hu.webuni.university.model.Course;
-import hu.webuni.university.model.HistoryData;
 import hu.webuni.university.repository.CourseRepository;
 import hu.webuni.university.service.CourseService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/courses")
-public class CourseController {
+public class CourseController implements CourseControllerApi {
 
 	private final CourseService courseService;
 	private final CourseRepository courseRepository;
 	
 	private final CourseMapper courseMapper;
-	
-	@PostMapping
-	public CourseDto createCourse(@RequestBody @Valid CourseDto courseDto) {
-		Course course = courseRepository.save(courseMapper.dtoToCourse(courseDto));
-		return courseMapper.courseToDto(course);
+	private final NativeWebRequest request;
+	private final MethodArgumentResolverHelper resolverHelper;
+
+	@Override
+	public Optional<NativeWebRequest> getRequest() {
+		return Optional.of(request);
 	}
+
+	@Override
+	public ResponseEntity<CourseDto> createCourse(@Valid CourseDto courseDto) {
+		Course course = courseRepository.save(courseMapper.dtoToCourse(courseDto));
+		return ResponseEntity.ok(
+				courseMapper.courseToDto(course)
+				);
+	}
+
+	@Override
+	public ResponseEntity<List<HistoryDataCourseDto>> getHistory(Integer id) {
+		return ResponseEntity.ok(
+			courseMapper.coursesHistoryToHistoryDataCourseDtos(courseService.getHistoryById(id))
+		);
+	}
+
+	@Override
+	public ResponseEntity<CourseDto> getVersionAt(Integer id, @NotNull @Valid OffsetDateTime at) {
+		return ResponseEntity.ok(courseMapper.courseToDto(courseService.getVersionAt(id, at)));
+	}
+
+	public void configPageable(@SortDefault("id") Pageable pageable) {}
 	
-	@GetMapping("/search")
-	public List<CourseDto> search(
-			@QuerydslPredicate(root = Course.class) Predicate predicate, 
-			@RequestParam Optional<Boolean> full,
-			@SortDefault("id") Pageable pageable){
-		boolean isFull = full.orElse(false);
+	public void configurePredicate(@QuerydslPredicate(root = Course.class) Predicate predicate) {}
+	
+	@Override
+	public ResponseEntity<List<CourseDto>> search(@Valid Boolean full, @Valid Integer page, @Valid Integer size,
+			@Valid List<String> sort) {
+		boolean isFull = full == null ? false : full;
+		
+		Pageable pageable = resolverHelper.createPageable(this.getClass(), "configPageable", request);
+
+		Predicate predicate = resolverHelper.createPredicate(this.getClass(), "configurePredicate", request);
 		if(isFull) {
-			Iterable<Course> courses = courseService.searchWithRelationships(predicate, pageable);
-			return courseMapper.coursesToDtos(courses);
+			Iterable<Course> courses = courseService.searchWithRelationships(
+					predicate, 
+					pageable);
+			return ResponseEntity.ok(courseMapper.coursesToDtos(courses));
 		} else {
 			Iterable<Course> courses = courseRepository.findAll(predicate, pageable);
-			return courseMapper.courseSummariesToDtos(courses);
+			return ResponseEntity.ok(courseMapper.courseSummariesToDtos(courses));
 		}
 	}
 	
-	@GetMapping("/history/{id}")
-	public List<HistoryData<CourseDto>> getHistory(@PathVariable int id){
-		return courseMapper.coursesToHistoryData(courseService.getHistoryById(id));
-	}
-	
-	@GetMapping(value = "/history/{id}", params = "when")
-	public CourseDto getVersionAt(@PathVariable int id, @RequestParam OffsetDateTime when){
-		return courseMapper.courseToDto(courseService.getVersionAt(id, when));
-	}
 }
