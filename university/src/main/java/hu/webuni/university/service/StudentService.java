6e416ec2635a6e3d97633f549d0c19599c1da73e
imre-gabor <gabor@aut.bme.hu>
2022-02-16 17:04:02 +0100

    solve week3/task2: upload/downloade profile picture

--- university/src/main/java/hu/webuni/university/service/StudentService.java
+++ university/src/main/java/hu/webuni/university/service/StudentService.java
@@ -1,9 +1,22 @@
 package hu.webuni.university.service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import hu.webuni.university.model.Student;
 import hu.webuni.university.repository.StudentRepository;
@@ -17,7 +30,20 @@ public class StudentService {
 	
 	private final StudentRepository studentRepository;
 	private final CentralEducationService centralEducationService;
+	
+	@Value("${university.content.profilePics}")
+	private String profilePicsFolder;
 
+	@PostConstruct
+	public void init() {
+		try {
+			Files.createDirectories(Path.of(profilePicsFolder));
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
 	@Scheduled(cron="${university.freeSemesterUpdater.cron}")
 	public void updateFreeSemesters() {
 		List<Student> students = studentRepository.findAll();
@@ -36,5 +62,27 @@ public class StudentService {
 				log.error("Error calling central education service.", e);
 			}
 		});
+	}
+
+	public void saveProfilePicture(Integer id, InputStream is) {
+		if(!studentRepository.existsById(id))
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		try {
+			Files.copy(is, getProfilePicPathForStudent(id), StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+	private Path getProfilePicPathForStudent(Integer id) {
+		return Paths.get(profilePicsFolder, id.toString() + ".jpg");
+	}
+
+	public Resource getProfilePicture(Integer studentId) {
+		FileSystemResource fileSystemResource = new FileSystemResource(getProfilePicPathForStudent(studentId));
+		if(!fileSystemResource.exists())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		return fileSystemResource;
 	}
 }
