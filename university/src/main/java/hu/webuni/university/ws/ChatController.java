83179ec53744c72d83d69ae5fc303c7de9259245
imre-gabor <gabor@aut.bme.hu>
2022-02-17 12:00:09 +0100

    solve week4/task4: websocket chat

---
+++ university/src/main/java/hu/webuni/university/ws/ChatController.java
@@ -0,0 +1,21 @@
+package hu.webuni.university.ws;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class ChatController {
+	
+	private final SimpMessagingTemplate messagingTemplate;
+
+	@MessageMapping("/chat")
+	public void send(ChatMessage message) throws Exception {
+		messagingTemplate.convertAndSend("/topic/courseChat/" + message.getCourseId(), 
+				String.format("%s: %s", message.getSender(), message.getText()));
+	}
+
+}
